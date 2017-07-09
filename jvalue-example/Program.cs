cb145d258c09bdaaf818b2e6be9b010aa354a4c1
Stuart Preston <stuart@chef.io>
2017-07-09 18:47:35 +0100

    Adding example code

---
+++ jvalue-example/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace jvalue_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonString = @"{""Firstname"":""Stuart"",""Surname"":""Preston""}";
+            dynamic json = JValue.Parse(jsonString);
+            Console.WriteLine("Hello {0} {1}", json.Firstname, json.Surname);
+        }
+    }
+}
